934a066c4431225b83cb384b677eeff09edc4c31
tienkma <tientung4700@gmail.com>
2023-04-12 21:53:44 +0700

    no message

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,6 +5,11 @@ import { Link } from "react-router-dom";
 import { Toasts } from "../utils/notification";
 import { isEmpty } from "lodash";
 import { Button } from "@mui/material";
+import { useForm, Controller } from "react-hook-form"
+import Labels from "../commons/label";
+import { yupResolver } from '@hookform/resolvers/yup';
+
+
 
 const LoginPage = () => {
   const [displayLogin, setDisplayLogin] = useState(true);
@@ -31,19 +36,25 @@ const SignIn = ({ setDisplayLogin }: any) => {
   // const { setUsers } = UseUserContext();
   // const { getListCart } = useCartContext();
 
-  const signInSchema = Yup.object({
-    email: Yup.string()
-      .email("Email must be a valid email")
-      .required("Please enter the value Email"),
-    password: Yup.string().required().min(8, "Your password is too short."),
-  });
-
+  const {
+    register,
+    handleSubmit,
+    formState,
+    control,
+  } = useForm({
+    resolver: yupResolver(Yup.object({
+      email: Yup.string()
+        .email("Email must be a valid email")
+        .required("Please enter the value Email"),
+      password: Yup.string().required().min(8, "Your password is too short."),
+    })
+    ),
+  })
 
 
   // TODO user
-  const handleSubmit = async (value: any) => {
+  const onSubmit = async (value: any) => {
     try {
-
     } catch (error) {
       console.log(`authen ${error}`);
     }
@@ -58,55 +69,54 @@ const SignIn = ({ setDisplayLogin }: any) => {
       autoComplete="off"
       method="POST"
     >
-      <h2>SIGN IN</h2>
-      {/* <div className="form-error">
-        {!isEmpty(validation.errors) &&
-          isValidation &&
-          Object.entries(validation.errors).map((item) => (
-            <p key={item[0]}>{item[1]}</p>
-          ))}
+      <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-2 ring-purple-600 lg:max-w-xl">
+        <h1 className="text-3xl font-semibold text-center text-purple-700 underline uppercase decoration-wavy">
+          Sign in
+        </h1>
+        <form className="mt-6">
+          <div className="mb-2">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="password"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <a href="#" className="text-xs text-purple-600 hover:underline">
+            Forget Password?
+          </a>
+          <div className="mt-6">
+            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+              Login
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-8 text-xs font-light text-center text-gray-700">
+          {" "}
+          Don't have an account?{" "}
+          <a href="#" className="font-medium text-purple-600 hover:underline">
+            Sign up
+          </a>
+        </p>
       </div>
-      <div>
-        <input
-          type="text"
-          autoComplete="off"
-          value={validation.values.email}
-          onChange={(e: any) => {
-            if (isValidation) {
-              setIsValidation(false);
-            }
-            validation.setFieldValue("email", e.target.value);
-          }}
-          id="email"
-          name="email"
-          required
-        />
-        <label htmlFor="email">Email</label>
-      </div>
-      <div>
-        <input
-          type="password"
-          id="password"
-          value={validation.values.password}
-          onChange={(e) => {
-            if (isValidation) {
-              setIsValidation(false);
-            }
-            validation.setFieldValue("password", e.target.value);
-          }}
-          autoComplete="new-password"
-          name="password"
-          required
-        />
-        <label htmlFor="password">Password</label>
-      </div> */}
-      <button type="submit" >
-        Login
-      </button>
-      <h5>
-        Don't have an account?
-        <button onClick={() => setDisplayLogin(false)}>Register</button>
-      </h5>
+      <button onClick={() => setDisplayLogin(false)}>Register</button>
     </form>
   );
 };
@@ -119,16 +129,12 @@ export const signUnSchema = Yup.object({
     .required("Please enter the value Password"),
 });
 
-
 const SignUp = ({ setDisplayLogin }: any) => {
   const [isValidation, setIsValidation] = useState(false);
-
-
 
   const handleSubmit = async (value: any, helpers: any) => {
     setIsValidation(true);
     try {
-      
     } catch (error) {
       console.log(`authen ${error}`);
     }
@@ -145,64 +151,7 @@ const SignUp = ({ setDisplayLogin }: any) => {
         method="POST"
       >
         <h2>SIGN UP</h2>
-        <div className="form-error">
-          {/* {!isEmpty(validation.errors) &&
-            isValidation &&
-            Object.entries(validation.errors).map((item) => (
-              <p key={item[0]}>{item[1]}</p>
-            ))}
-        </div>
-        <div>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            value={validation.values.email}
-            autoComplete="off"
-            onChange={(e) => {
-              if (isValidation) {
-                setIsValidation(false);
-              }
-              validation.setFieldValue("email", e.target.value);
-            }}
-            required
-          />
-          <label htmlFor="email">Email</label>
-        </div>
-        <div>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={validation.values.name}
-            autoComplete="off"
-            onChange={(e) => {
-              if (isValidation) {
-                setIsValidation(false);
-              }
-              validation.setFieldValue("name", e.target.value);
-            }}
-            required
-          />
-          <label htmlFor="username">User name</label>
-        </div>
-        <div>
-          <input
-            type="password"
-            id="password"
-            value={validation.values.password}
-            autoComplete="off"
-            onChange={(e) => {
-              if (isValidation) {
-                setIsValidation(false);
-              }
-              validation.setFieldValue("password", e.target.value);
-            }}
-            name="password"
-            required
-          />
-          <label htmlFor="password">Password</label> */}
-        </div>
+        <div className="form-error"></div>
         <Button type="submit" onClick={() => setIsValidation(true)}>
           Register
         </Button>
