b34fd86c9653b123ce94b6be3f5df5e791b60a40
tienkma <tientung4700@gmail.com>
2023-06-13 00:35:27 +0700

    form comment

--- src/components/pages/products/Comments.tsx
+++ src/components/pages/products/Comments.tsx
@@ -3,6 +3,12 @@ import { useState } from "react";
 import { Storage } from "../../../utils/local";
 import { format } from "date-fns";
 import { AiOutlineSend } from "react-icons/ai";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+
+export const classNameInput =
+  "block w-full p-2 text-gray-900 border-gray-300 bg-gray-20 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-0 !border rounded-lg";
 
 interface CommentsProps {
   item: Record<string, any>;
@@ -61,6 +67,7 @@ interface CommentProps {
 
 const Comment = (props: CommentProps) => {
   const { comment } = props;
+  const [activeComment, setActiveComment] = useState(false);
   const [valueComment, setValueComment] = useState("");
 
   return (
@@ -212,47 +219,95 @@ const Comment = (props: CommentProps) => {
           <p className="text-gray-500 dark:text-gray-400">
             Much appreciated! Glad you liked it ☺️
           </p>
-          <div className="flex items-center mt-4 space-x-4">
-            <button
-              type="button"
-              className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400"
-            >
-              <svg
-                aria-hidden="true"
-                className="mr-1 w-4 h-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                ></path>
-              </svg>
-              Reply
+        </article>
+        {activeComment ? (
+          <div className=" ml-4 pr-0 lg:ml-8 mt-5">
+            <FormComment />
+          </div>
+        ) : (
+          <div
+            className={`relative  ml-4 pr-0 lg:ml-8 focus:block focus:first:block ${
+              !valueComment ? "hidden group-hover:block" : ""
+            } mt-5 `}
+          >
+            <input
+              type="text"
+              className="w-full rounded-lg"
+              placeholder="Enter your comment"
+              onFocus={() => setActiveComment(true)}
+              onChange={(e) => setValueComment(e.target.value)}
+            />
+            <button className="right-4 text-blue-500 !left-auto centerAb cursor-pointer">
+              <AiOutlineSend className=" " size={22} />
             </button>
           </div>
-        </article>
-        <div
-          className={`relative  ml-4 pr-0 lg:ml-8 focus:block focus:first:block ${
-            !valueComment ? "hidden group-hover:block" : ""
-          } mt-5 `}
-        >
-          <input
-            type="text"
-            className="w-full rounded-lg"
-            placeholder="Enter your comment"
-            onChange={(e) => setValueComment(e.target.value)}
-          />
-          <button className="right-4 text-blue-500 !left-auto centerAb cursor-pointer">
-            <AiOutlineSend className=" " size={22} />
-          </button>
-        </div>
+        )}
       </div>
     </article>
+  );
+};
+
+interface FormCommentProps {}
+
+const FormComment = (props: FormCommentProps) => {
+  const { register, handleSubmit, formState, control } = useForm({
+    resolver: yupResolver(
+      Yup.object({
+        email: Yup.string()
+          .email("Email must be a valid email")
+          .required("Please enter the value Email"),
+        password: Yup.string().required().min(8, "Your password is too short."),
+      })
+    ),
+  });
+
+  const onSubmit = (values: Record<string, string>) => {};
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex justify-between gap-5">
+        <input
+          {...register("name")}
+          type="text"
+          className={`text-sm bg-[#e5e5e5] ${classNameInput}`}
+          placeholder="Name"
+        />
+        <input
+          {...register("numberPhone")}
+          type="text"
+          className={`text-sm bg-[#e5e5e5] ${classNameInput}`}
+          placeholder="Number Phone"
+        />
+        <input
+          {...register("email")}
+          type="email"
+          className={`text-sm bg-[#e5e5e5] ${classNameInput}`}
+          placeholder="Email"
+        />
+      </div>
+      <div className="py-2 px-4 mb-4 bg-[#e5e5e5] mt-5 rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <label htmlFor="comment" className="sr-only">
+          Your comment
+        </label>
+        <textarea
+          id="comment"
+          rows={6}
+          className="px-0  w-full text-sm bg-[#e5e5e5] text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+          placeholder="Write a comment..."
+          required
+        ></textarea>
+      </div>
+      <div className="mt-4 flex justify-between items-center">
+        <i className="text-sm">
+          To post a comment, you need to enter at least the name and content
+          fields
+        </i>
+        <button className="right-4 text-white items-center text-sm !left-auto flex cursor-pointer bg-background hover:bg-color transition-colors px-10 py-2 rounded-3xl">
+          <AiOutlineSend className="text-sm mr-1" size={16} />
+          Post Comment
+        </button>
+      </div>
+    </form>
   );
 };
 
